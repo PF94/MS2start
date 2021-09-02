dee2ccc3ce4204c8bc57052003bff31e03c554d4
Gamerappa <chazpelo2@gmail.com>
2021-09-01 21:48:13 -0400

    add amazing comment

--- StartScreen/MainWindow.xaml.cs
+++ StartScreen/MainWindow.xaml.cs
@@ -31,6 +31,10 @@ namespace StartScreen
             string[] pets = { "dog", "cat", "bird" }; //placeholder until i make proper settings
             foreach (string value in pets)
             {
+                //todo: load icon from executable file WITHOUT DUMPING IT TO A CACHE FOLDER AND WITHOUT DOWNGRADING BACK TO NET FRAMEWORK 4.X
+                //nativemethod is absolutely fucking broken and i am not going to use winforms for this bullshit. the ms2start winform code is
+                //hardcode garbage, not going to use it. this is like returning back to poktube because there's a issue with the squarebracket code.
+                // -gr 9/1/2021
                 Tile startTile = new Tile();
                 startTile.label.Content = value;
                 panelEpic.Children.Add(startTile);
